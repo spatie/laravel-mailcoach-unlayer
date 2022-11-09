cfa1d7ecbea530216a2aa48fd7fd2ff700848f07
riasvdv <rias@spatie.be>
2022-11-09 10:58:43 +0000

    Update split workflow

--- tests/UnlayerEditorTest.php
+++ tests/UnlayerEditorTest.php
@@ -2,22 +2,17 @@
 
 namespace Spatie\MailcoachUnlayer\Tests;
 
+use Livewire\Livewire;
 use Spatie\Mailcoach\Domain\Campaign\Models\Template;
-use Spatie\MailcoachUnlayer\UnlayerEditor;
 
 class UnlayerEditorTest extends TestCase
 {
     /** @test * */
     public function it_renders_a_view()
     {
-        $editor = new UnlayerEditor();
-
         $template = Template::factory()->create();
-
-        $html = $editor->render($template);
-
-        $this->assertStringContainsString('input type="hidden" name="html"', $html);
-        $this->assertStringContainsString('input type="hidden" name="structured_html"', $html);
+        Livewire::test('mailcoach-unlayer::editor', ['model' => $template])
+            ->assertSee('unlayer.init');
     }
 
     /** @test * */
@@ -27,13 +22,9 @@ class UnlayerEditorTest extends TestCase
             'appearance' => ['theme' => 'dark'],
         ]]);
 
-        $editor = new UnlayerEditor();
-
         $template = Template::factory()->create();
-
-        $html = $editor->render($template);
-
-        $this->assertStringContainsString('appearance', $html);
-        $this->assertStringContainsString('dark', $html);
+        Livewire::test('mailcoach-unlayer::editor', ['model' => $template])
+            ->assertSee('appearance')
+            ->assertSee('dark');
     }
 }
